1eb99ce488dc348dbc1d029726e2ade9b31119d0
abdalrahmansamara <abdsamara_1998@yahoo.com>
2021-06-09 21:52:59 +0300

    commit 1

---
+++ README.md
@@ -0,0 +1,9 @@
+Description: Welcome to pythonic garage band, a functionality that allows you to create your band, have a special catch frases, and even play sole!
+
+Problem Domain: you should create a five classes that are responsible for organising and ordering bands, musicians and give them some catch frases and lines to play solo
+
+Start time : 7:00 pm
+
+Finish time: 9:20 pm
+
+Notes: i learned alot of new functionalities while solving this lab
---
+++ pythonic_garage_band/pythonic_garage_band.py
@@ -0,0 +1,87 @@
+from abc import abstractmethod
+
+class Band():
+    instances = []
+    def __init__(self,name,arr):
+        self.name=name
+        self.members=arr
+        Band.instances.append(self)
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+    def play_solos(self):
+        solos=[]
+        for i in self.members:
+            solos.append(i.play_solo())
+        return solos
+
+
+class Musician():
+     @abstractmethod
+     def __str__(self):
+         pass
+     @abstractmethod
+     def __repr__(self):
+         pass
+     @abstractmethod
+     def get_instrument(self):
+         pass
+     @abstractmethod
+     def play_solo(self):
+         pass
+
+class Guitarist(Musician):
+    def __init__(self,name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play guitar"
+    def __repr__(self):
+        return f"Guitarist instance. Name = {self.name}"
+    @staticmethod
+    def get_instrument():
+        return "guitar"
+    @staticmethod
+    def play_solo():
+        return "face melting guitar solo"
+        
+
+# return super().get_instrument()
+
+class Drummer(Musician):
+    def __init__(self,name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+    @staticmethod
+    def get_instrument():
+        return "drums"
+    @staticmethod
+    def play_solo():
+        return "rattle boom crash"
+    
+
+class Bassist(Musician):
+    def __init__(self,name):
+        self.name = name
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+    @staticmethod
+    def get_instrument():
+        return "bass"
+    @staticmethod
+    def play_solo():
+        return "bom bom buh bom"
+    
+
+# members = [
+#     Guitarist("Kurt Cobain"),
+#     Bassist("Krist Novoselic"),
+#     Drummer("Dave Grohl"),
+# ]
+
+# some_band = Band("Nirvana", members)
+# some_band.play_solos()
